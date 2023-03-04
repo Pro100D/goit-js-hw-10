274c56d98798c4286437b253768f48fcde0d627c
Danil Takiylin <danadanil900@gmail.com>
2023-03-04 14:59:32 +0100

    проверка js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import './css/styles.css';
+// import { fetchCountries } from './js/fetchCountries';
+// import Notiflix from 'notiflix';
+
+// var debounce = require('lodash.debounce');
+
+// const DEBOUNCE_DELAY = 300;
+
+// const inputSearch = document.querySelector('#search-box');
+
+// inputSearch.addEventListener(
+//   'input',
+//   debounce(evt => {
+//     const inputSearch = input.value.trim();
+//     if (inputSearch !== '') {
+//       fetchCountries(inputSearch.value).then(country => {
+//         if (country.length > 10) {
+//           Notiflix.Notify.info(
+//             'Too many matches found. Please enter a more specific name.'
+//           );
+//         }
+//       });
+//     }
+//   }, DEBOUNCE_DELAY)
+// );
